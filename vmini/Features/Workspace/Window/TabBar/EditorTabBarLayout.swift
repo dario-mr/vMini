b2343f37b766a07f04d2eb2640d2f467d206360a
dario-mr <dario.mauri9@gmail.com>
2026-07-27 17:16:56 +0200

    tweak: UI tab height

--- vmini/Features/Workspace/Window/TabBar/EditorTabBarLayout.swift
+++ vmini/Features/Workspace/Window/TabBar/EditorTabBarLayout.swift
@@ -3,7 +3,7 @@ import AppKit
 enum EditorTabBarLayout {
     static let minimumTabWidth: CGFloat = 160
     static let maximumTabWidth: CGFloat = 240
-    static let tabBarHeight: CGFloat = 30
-    static let tabHeight: CGFloat = 28
+    static let tabBarHeight: CGFloat = 32
+    static let tabHeight: CGFloat = 30
     static let tabBarHorizontalInset: CGFloat = 2
 }
